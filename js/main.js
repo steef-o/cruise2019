a50c2c30b1bc2cd21da5316ed251e29efc0702a3
Steffen Andre Hagen <steffen.a.hagen@gmail.com>
2019-08-28 21:06:13 +0200

    changes to css

--- js/main.js
+++ js/main.js
@@ -1,54 +1,57 @@
 "use strict";
 
 const search = document.getElementById("search");
-const searchResults = document.getElementById("search-results");
+const searchResults = document.getElementById("search-results--content");
 const selectionType = document.getElementById("selection-type");
 let calendar;
 
 //Search ships.json and filter it.
 const searchForShips = async searchText => {
-    const res = await fetch ('data/ships.json');
-    const ships = await res.json();
+  const res = await fetch("data/ships.json");
+  const ships = await res.json();
 
+  //Get matches to current text input
+  let matches = ships.filter(ship => {
+    const regex = new RegExp(`^${searchText}`, "gi");
+    return ship.name.match(regex) || ship.date.match(regex);
+  });
 
-    //Get matches to current text input
-    let matches = ships.filter(ship => {
-        const regex = new RegExp(`^${searchText}`, 'gi');
-        return ship.name.match(regex) || ship.date.match(regex);
-    });
+  if (searchText.length === 0) {
+    matches = [];
+    searchResults.innerHTML = "";
+  }
 
-    if(searchText.length === 0){
-        matches = [];
-        searchResults.innerHTML= '';
-    }
-
-    console.log(searchText, "<-- searchtext");
-
-
-    outputTimeline(matches);
+  // Debug
+  // console.log(searchText, '<-- searchtext');
+  outputTimeline(matches);
 };
 
 // Show results in HTML
-const outputHTML = matches => {
-    if(matches.length > 0){
-        const html = matches.map(match => `
+/*const outputHTML = matches => {
+  if (matches.length > 0) {
+    const html = matches
+      .map(
+        match => `
             <div class="box ship-card">
                <i class="fas fa-ship"></i>
                <h4>${match.name} (${match.date}) <span class="has-text-primary">${match.day}</span></h4> 
                <small>Max passengers: ${match.max_pax} / nationality: ${match.nationality}</small>
             </div>
-         `).join('');
+         `
+      )
+      .join("");
 
-        console.log(html);
-        searchResults.innerHTML = html;
-    }
-};
-
+    // console.log(html);
+    searchResults.innerHTML = html;
+  }
+};*/
 
 const outputTimeline = matches => {
-    if(matches.length > 0){
-        const html = matches.map(match =>
-            `
+  if (matches.length > 0) {
+    const html = matches
+      .map(
+        match =>
+          `
             <div class="timeline">
                 <header class="timeline-header">
                     <span class="tag is-medium is-info">${match.date}</span>
@@ -63,57 +66,57 @@ const outputTimeline = matches => {
                     </div>
                 </div>
             </div>
-        `).join('');
+        `
+      )
+      .join("");
 
-        searchResults.innerHTML = html;
-    }
+    searchResults.innerHTML = html;
+  }
 };
 
 const changeInputType = () => {
-    let selection = document.getElementById("selection-type");
-    search.setAttribute("type", selection.type);
-    if(selection.value === "date"){
-        search.setAttribute("placeholder", "Velg dato");
-        calendar = initCalendar();
-
-    } else {
-        search.setAttribute("placeholder", "Søk etter skipsnavn");
-        if(calendar !== undefined){
-            calendar.destroy();
-        }
+  let selection = document.getElementById("selection-type");
+  search.setAttribute("type", selection.type);
+  if (selection.value === "date") {
+    search.setAttribute("placeholder", "Velg dato");
+    calendar = initCalendar();
+  } else {
+    search.setAttribute("placeholder", "Søk etter skipsnavn");
+    if (calendar !== undefined) {
+      calendar.destroy();
     }
+  }
 };
-
 
 const initCalendar = () => {
-   return flatpickr("#search",{
-        enableTime: false,
-        altInput: true,
-        altFormat: "j F, Y",
-        dateFormat: "d.m.Y",
-    });
+  return flatpickr("#search", {
+    enableTime: false,
+    altInput: true,
+    altFormat: "j F, Y",
+    dateFormat: "d.m.Y"
+  });
 };
-
 
 const setTodaysShip = () => {
-    const now = new Date();
-    const searchDate = dateFormat(now, "dd.mm.yyyy");
-    searchForShips(searchDate);
+  const now = new Date();
+  const searchDate = dateFormat(now, "dd.mm.yyyy");
+  searchForShips(searchDate);
 };
 
-const formatDateFromJSON = (str) => {
-    const date = new Date(formatAndReverseString(str));
-    return dateFormat(new Date(date), "mmmm");
+const formatDateFromJSON = str => {
+  const date = new Date(formatAndReverseString(str));
+  return dateFormat(new Date(date), "mmmm");
 };
 
-    const formatAndReverseString = (str) => {
-    return str.split('.').reverse().join('-');
+const formatAndReverseString = str => {
+  return str
+    .split(".")
+    .reverse()
+    .join("-");
 };
 
 setTodaysShip();
-selectionType.addEventListener('change', () => changeInputType());
-search.addEventListener('input', () => { searchForShips(search.value); });
-
-
-
-
+selectionType.addEventListener("change", () => changeInputType);
+search.addEventListener("input", () => {
+  searchForShips(search.value);
+});
